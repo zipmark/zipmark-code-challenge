c0d7de60e48ae1d625b6bc20cd4270bddd4f415b
jbmilgrom <jbmilgrom@gmail.com>
2014-08-10 14:25:08 -0400

    using get '/banks' to test parsing

--- main.rb
+++ main.rb
@@ -3,8 +3,11 @@ require 'sinatra/reloader'
 require 'httparty'
 require 'pry'
 
+URL = "https://gist.githubusercontent.com/jakehow/3d96b83c4a4a1e6313d4/raw/f2990b4cef86fe9454ba1a643ccee118ca0b5757/truncated_list"
 
 get "/banks" do
+  @banks = HTTParty.get(URL)
+
   erb :"banks/index"
 end
 
