aa0b95d391bfcea5ed82a51cb90c478cbc32b685
jbmilgrom <jbmilgrom@gmail.com>
2014-08-11 15:54:29 -0400

    cleaning up

--- main.rb
+++ main.rb
@@ -7,8 +7,6 @@ require_relative 'data_processor'
 
 URL = "https://gist.githubusercontent.com/jakehow/3d96b83c4a4a1e6313d4/raw/f2990b4cef86fe9454ba1a643ccee118ca0b5757/truncated_list"
 
-# include ProcessMethods
-
 get "/banks" do
   
   content_type :json
