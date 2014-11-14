a78261e51b24d9e3d9f0e592812152966d172a68
Dylan Krause <dykrause@gmail.com>
2014-11-14 00:15:14 -0500

    Add complete migration for banks table creation and its schema

--- db/migrate/20141114050624_create_banks.rb
+++ db/migrate/20141114050624_create_banks.rb
@@ -1,7 +1,14 @@
 class CreateBanks < ActiveRecord::Migration
   def change
     create_table :banks do |t|
-
+      t.string :name
+      t.string :routing_number
+      t.string :record_type
+      t.string :street
+      t.string :street_optional
+      t.string :city
+      t.string :state
+      t.string :zip_code
       t.timestamps
     end
   end
