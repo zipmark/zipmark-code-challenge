3e8e0ae2e4b32589222b0701b3e6b4cdec05d1b4
Dylan Krause <dykrause@gmail.com>
2014-11-14 01:30:42 -0500

    Add class attribute to BankReader class that represents default indices of values on a string for bank info -key is attribute name -value is range

--- app/models/banks_reader.rb
+++ app/models/banks_reader.rb
@@ -1,6 +1,18 @@
 require 'open-uri'
 class BanksReader
 
+  @@default_specs = {
+    routing_number: 0..8,
+    record_type: 19,
+    change_date: 20..25,
+    updated_routing: 26..34,
+    name: 35..70,
+    address: 71..106,
+    city: 107..126,
+    state: 127..128,
+    zip_code: 129..133
+  }
+
   def self.ach_dir(url= "http://www.fededirectory.frb.org/FedACHdir.txt")
     text = URI.parse(url).read.split("\n")
   end
