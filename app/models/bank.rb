dde84241e58464d1e2bd198654d170bc0a199fec
Dylan Krause <dykrause@gmail.com>
2014-11-14 13:53:48 -0500

    Remove state presence validation for foreign banks

--- app/models/bank.rb
+++ app/models/bank.rb
@@ -1,7 +1,7 @@
 class Bank < ActiveRecord::Base
   validates :routing_number, uniqueness: true
   validates :routing_number, length: { is: 9 }
-  validates :name, :record_type, :street, :city, :state, :zip_code,  presence: true
+  validates :name, :record_type, :street, :city, :zip_code,  presence: true
 
   before_save :titleize_attrs
 
