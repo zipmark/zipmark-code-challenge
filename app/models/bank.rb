0ad1f9a2d31b6ca14b1b63410c05c32a551cc990
Dylan Krause <dykrause@gmail.com>
2014-11-14 12:01:08 -0500

    Add formatting method on Bank Model to allow for more readable json response

--- app/models/bank.rb
+++ app/models/bank.rb
@@ -10,4 +10,32 @@ class Bank < ActiveRecord::Base
   self.city= city.titleize
  end
 
+  def formatted
+    response = {}
+    response[:name] = name
+    response[:routing_number] = routing_number
+    response[:record_type] = record_type
+    response[:address] = {}
+    response[:address][:street] = street
+    response[:address][:street_2] = street_optional unless street_optional.nil?
+    response[:address][:city] = city
+    response[:address][:zip_code] = zip_code
+    response[:updated_routing_number] = updated_routing_number unless updated_routing_number.nil?
+    response
+  end
 end
+
+
+{
+  name: "First Bank of Zipmark",
+  routing_number: "123456789",
+  record_type: "Record Type",
+  phone_number: "212-555-1212",
+  address: {
+    street: "56 W 22nd St",
+    street_2: "9th Floor",
+    city: "New York",
+    state: "NY",
+    zip_code: "10010"
+  }
+}
