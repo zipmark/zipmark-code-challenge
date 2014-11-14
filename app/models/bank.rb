7b61f45ece2a3abacd3a042e09a8e15d66468b28
Dylan Krause <dykrause@gmail.com>
2014-11-14 10:52:25 -0500

    Add presence validations to bank attributes

--- app/models/bank.rb
+++ app/models/bank.rb
@@ -1,5 +1,6 @@
 class Bank < ActiveRecord::Base
   validates :routing_number, uniqueness: true
   validates :routing_number, length: { is: 9 }
+  validates :name, :record_type, :street, :city, :state, :zip_code,  presence: true
 
 end
