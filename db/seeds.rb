d16b67be004320cf8c8574098a53cae1868c9a41
Dylan Krause <dykrause@gmail.com>
2014-11-14 10:54:31 -0500

    Add seeding method that parses fed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,7 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+listings = BanksReader.ach_dir
+bank_hashes = BanksReader.hash_banks(listings)
+bank_hashes.each do |bank_hash|
+  p bank_hash
+  bank = BanksReader.format_bank(bank_hash)
+  Bank.create(bank)
+end
