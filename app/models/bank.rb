0c61b2399d873c92d27b983247fa300fb5b1295d
Dylan Krause <dykrause@gmail.com>
2014-11-14 10:53:11 -0500

    Add titleize_attr method to bank model and call it before saving -only covers cities for now -needs to be optimized for dealing with irregular fed data in names and addresses

--- app/models/bank.rb
+++ app/models/bank.rb
@@ -3,4 +3,11 @@ class Bank < ActiveRecord::Base
   validates :routing_number, length: { is: 9 }
   validates :name, :record_type, :street, :city, :state, :zip_code,  presence: true
 
+  before_save :titleize_attrs
+
+
+ def titleize_attrs
+  self.city= city.titleize
+ end
+
 end
