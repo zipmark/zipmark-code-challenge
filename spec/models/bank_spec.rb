cffd183990842a8a2c428d79a0afc320108c64ec
Dylan Krause <dykrause@gmail.com>
2014-11-14 09:54:43 -0500

    Add tests for routing number validations on bank model

--- spec/models/bank_spec.rb
+++ spec/models/bank_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Bank, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it "expects to have a unique routing number" do
+    bank1 = create(:bank, routing_number: "123456789")
+    bank2 = build(:bank, routing_number: "123456789")
+    expect(bank2).to be_invalid
+  end
+
+  it "is invalid without a 9 digit routing number" do
+    bank = build(:bank, routing_number: 12345678)
+    expect(bank).to be_invalid
+  end
 end
