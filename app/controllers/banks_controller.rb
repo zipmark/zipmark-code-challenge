8d53d8f96e3368509466b532dca739d59b7ff199
Dylan Krause <dykrause@gmail.com>
2014-11-14 12:02:31 -0500

    Add three helper methods to the bank show controller route -load bank by routing number -format bank as a hash -respond with bank as a json object

--- app/controllers/banks_controller.rb
+++ app/controllers/banks_controller.rb
@@ -1,7 +1,26 @@
 class BanksController < ApplicationController
 
   def show
+    load_bank_by_route
+    format_bank
+    send_bank
+  end
 
+  private
+  def load_bank_by_route
+    @bank ||= Bank.find_by(routing_number: params[:routing_number])
+  end
+
+  def format_bank
+    if @bank
+      @bank = @bank.formatted
+    else
+      @bank = "Bank Not Found/Routing Number Invalid"
+    end
+  end
+
+  def send_bank(template= :json)
+    render template => @bank
   end
 
 end
