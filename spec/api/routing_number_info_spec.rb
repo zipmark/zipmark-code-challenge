0eab0e71825dba063bb3c79108952241c4847b65
Dylan Krause <dykrause@gmail.com>
2014-11-16 13:15:27 -0500

    Write spec for routing to basic bank object api

--- spec/api/routing_number_info_spec.rb
+++ spec/api/routing_number_info_spec.rb
@@ -3,6 +3,22 @@ require 'rails_helper'
 RSpec.describe "Bank Routing Number API", :type => :request do
 
   it "displays displays a json object with basic attributes" do
+    bank = create(:bank, routing_number: "123456789", street_optional: nil, updated_routing_number: nil)
+    expected = { name: bank.name,
+                 routing_number: bank.routing_number,
+                 record_type: bank.record_type,
+                 phone_number: bank.phone_number,
+                 address: {
+                            street: bank.street,
+                            city: bank.city,
+                            state: bank.state,
+                            zip_code: bank.zip_code
+                 }
+
+    }.to_json
+    get "/banks/123456789"
+    expect(response.body).to eq(expected)
+
   end
 
   it "displays displays a json object with a street_2 attribute if the said attribute on model is not null" do
