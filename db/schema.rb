d8f913dd360a633f27b4ce87b7227a3407d208c9
Dylan Krause <dykrause@gmail.com>
2014-11-16 13:28:38 -0500

    Add migration for phone attributes and updated schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141114063704) do
+ActiveRecord::Schema.define(version: 20141116175605) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20141114063704) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "updated_routing_number"
+    t.string   "phone_number"
   end
 
 end
