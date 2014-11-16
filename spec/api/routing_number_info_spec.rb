551183651fc65cef3694fe4d05b6323cbea45839
Dylan Krause <dykrause@gmail.com>
2014-11-16 12:17:42 -0500

    Add outline for request specs that deal with the fundamental routing number api on the banks/routing_number route

---
+++ spec/api/routing_number_info_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "Bank Routing Number API", :type => :request do
+
+  it "displays displays a json object with basic attributes" do
+  end
+
+  it "displays displays a json object with a street_2 attribute if the said attribute on model is not null" do
+  end
+
+  it "displays displays a json object with an updated_routing_number attribute if the said attribute on model is not null" do
+  end
+
+end
