77fb088db03c2ed292cc783e35b6cb4dcb26a53a
Ambi Sidhu <ambi@boxbee.com>
2015-08-03 12:35:35 -0400

    migration for banks

---
+++ spec/factories/banks.rb
@@ -0,0 +1,14 @@
+FactoryGirl.define do
+  factory :bank do
+    name ""
+routing_number ""
+record_type ""
+phone_number ""
+street ""
+street2 ""
+city ""
+state ""
+zip_code "MyString"
+  end
+
+end
